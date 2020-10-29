ab60aa820f18b6aa85d54dc79bfd3df54a02fb8c
Will Jessop <wjessop@gmail.com>
2020-10-28 20:22:17 -0400

    Build 2.0.0-201. Added example app.

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+import KustomerChat
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func showSupport(){
+    Kustomer.show()
+  }
+
+}
+
